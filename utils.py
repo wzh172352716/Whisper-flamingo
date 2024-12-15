b915ff43d751c17dfa5b4ecc278028fef5c88653
zer <--global wzh172352716@gmail.com>
2024-12-15 14:00:27 +0100

    init

--- utils.py
+++ utils.py
@@ -211,8 +211,8 @@ def add_noise(clean_wav, noise_wavs, noise_snr=0):
     return mixed
 
 def load_data(AUDIO_MAX_LENGTH, TEXT_MAX_LENGTH, langs=['en', 'ar', 'de', 'el', 'es', 'fr', 'it', 'pt', 'ru'],
-              muavic_root='/data/sls/scratch/roudi/datasets/muavic/', reduce_val=None, include_audio_lens=False,
-              AUDIO_MAX_LENGTH_VAL=480000, vc2=False, vc2_path='', lrs2=False, visible=False, task='transcribe'):
+              muavic_root='/beegfs/data/shared/muavic/muavic', reduce_val=None, include_audio_lens=False,
+              AUDIO_MAX_LENGTH_VAL=480000, translate=False, lrs2=False):
     # reduce_val: If not None, keep this number of samples from the validation set
     audio_transcript_pair_list = {'train':[], 'valid':[], 'test':[]}
     for lang in langs:
